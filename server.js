287b7054a40ebc8912ba400273d6b9e03a9db2ca
Sanjar (SAM) <sanjarberdimurodov19915@gmail.com>
2026-05-24 21:10:31 +0900

    fix: modifying reja project

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 const http = require("http");
 const { MongoClient } = require("mongodb");
 const app = require("./app");
+require("dotenv").config();
 
 // 🔴 Atlas’dagi database nomi bilan AYNAN bir xil bo‘lsin
 const DB_NAME = "Reja";
 
-const CONNECTION_STRING =
-  "mongodb://sanjar_sam_dev_1191:HOHvYcMPnQQAHVba@ac-tfux50z-shard-00-00.fthdmjx.mongodb.net:27017,ac-tfux50z-shard-00-01.fthdmjx.mongodb.net:27017,ac-tfux50z-shard-00-02.fthdmjx.mongodb.net:27017/Reja?ssl=true&replicaSet=atlas-qzcrmq-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
+const CONNECTION_STRING = process.env.MONGO_URI;
 
 const client = new MongoClient(CONNECTION_STRING);
 
@@ -20,7 +20,7 @@ async function start() {
     app.locals.db = client.db(DB_NAME);
 
     const server = http.createServer(app);
-    const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT || 3010;
 
     server.listen(PORT, () => {
       console.log("Web server starts");
